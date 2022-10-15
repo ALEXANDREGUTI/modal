298ab7e4edb41cf5d802e43d0962c59b3d8c9e87
Alexandre Gutierrez <gutierreztransporte@gmail.com>
2022-10-15 11:56:43 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton= document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+    [modal, fade].forEach((el) => el.classList.toggle("hide"));
+     
+};
+
+[openModalButton, closeModalButton, fade].forEach((el)=> {
+    el.addEventListener("click", ()=> toggleModal());
+});
